5d475ecae270c449419963f60d1c2105dcc932c8
tae2089 <tae2089002@gmail.com>
2021-05-08 01:44:04 +0900

    week1 자바 코드 타입공부

---
+++ src/main/java/week1/Hello.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class Hello {
+    public static void main(String[] args) {
+        //integer에 대해서
+            int a = 0;
+        // String 에 대해서
+            String b = "";
+        // Bool 에 대해서
+            boolean bool = false;
+        // Hello
+            System.out.println("");
+        
+    }
+}
---
+++ src/main/java/week1/Main.java
@@ -0,0 +1,32 @@
+package week1;
+
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
+public class Main {
+    public static void main(String[] args){
+        // 논리형
+            boolean testBoolean = true;
+            // testBoolean = false;
+        // 문자형
+            char c = 'a';
+            String testString  = "asd1qweqwrt";
+        // 정수형
+            byte b = 1;
+            short testshort = 2;
+            int testInteger = 3;
+            long testlong = 4;
+        //실수형
+            float testfloat = 5.0f;
+            double testdouble = 6.0;
+            final int PI = 1;
+            // PI = 12;
+            // Immutable -> 바뀌지 않는다 value가
+            String a = "sss";
+            a+="11111";
+            String newString = "sss11111";
+            int testAutoTypeCasting = 5;
+            long testLoginAutoTypeCasting = testAutoTypeCasting;
+            testAutoTypeCasting = (int)testLoginAutoTypeCasting;
+            
+    }
+}
